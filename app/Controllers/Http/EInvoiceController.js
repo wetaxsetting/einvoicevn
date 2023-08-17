792196b96da1287876f71f30736ac0f08079d457
lvthezh <lvthe.zh@gmail.com>
2023-08-17 14:26:42 +0700

    000

--- app/Controllers/Http/EInvoiceController.js
+++ app/Controllers/Http/EInvoiceController.js
@@ -4491,7 +4491,8 @@ class EInvoiceController {
             let EiExcel = new EiPosExcelHandler();
             let url_pdf = await EiExcel.getEinvoice(tei_wt_sale_bill_pk , p_language, p_crt_by);
             console.log("base64PDf  ", url_pdf);
-            let url_xml = await Request.get( APP_URL_LOCAL+"/api/dso/getfiledbtoken?pk=" + tei_wt_sale_bill_pk + "&proc=" + "EI_SEL_XML_POS_EINVOICE" + "&token=");//  await this.getUrlXML(tei_wt_sale_bill_pk, "EI_SEL_XML_POS_EINVOICE" );
+            let re_url_xml = await Request.get( APP_URL_LOCAL+"/api/dso/getfiledbtoken?pk=" + tei_wt_sale_bill_pk + "&proc=" + "EI_SEL_XML_POS_EINVOICE" + "&token=");//  await this.getUrlXML(tei_wt_sale_bill_pk, "EI_SEL_XML_POS_EINVOICE" );
+            let url_xml = re_url_xml.data;
             console.log("base64XXML  ", url_xml);
 
             const res_send_mail = await Request.post(EINVOICE_API_SEND_MAIL, {

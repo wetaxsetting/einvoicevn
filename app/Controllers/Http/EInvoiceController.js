91f7823394f6604fe7faad203b1b74e51991241a
lvthe.zh <lvthe.zh@gmail.com>
2024-04-11 16:39:49 +0700

    update

--- app/Controllers/Http/EInvoiceController.js
+++ app/Controllers/Http/EInvoiceController.js
@@ -7077,7 +7077,7 @@ class EInvoiceController {
         p_logo_height: logo_height,
         p_logo_start_col: '0.5',
         p_logo_start_row: '1.7',
-		p_status : status				 
+		    p_status : status				 
       };
 
       const rtnValue = await DBService.ExecuteSQLBlob(
@@ -7095,6 +7095,7 @@ class EInvoiceController {
                                                           :p_logo_height,
                                                           :p_logo_start_row,
                                                           :p_logo_start_col,
+                                                          :p_status,
                                                           :p_language, 
                                                           :p_crt_by, 
                                                           :p_rtn_cur); END;`,
@@ -7329,6 +7330,7 @@ class EInvoiceController {
                                                             :p_logo_height,
                                                             :p_logo_start_row,
                                                             :p_logo_start_col,
+                                                            :p_status,
                                                             :p_language, 
                                                             :p_crt_by, 
                                                             :p_rtn_cur); END;`,

9020c40e57ce174d559fe4c076cd2bc7e0523c0c
lvthe.zh <lvthe.zh@gmail.com>
2024-06-06 09:30:51 +0700

    sss

--- app/Controllers/Http/EInvoiceController.js
+++ app/Controllers/Http/EInvoiceController.js
@@ -6801,7 +6801,7 @@ class EInvoiceController {
               p_language,
               p_crt_by,
             );
-
+            console.log('res_send_mail weTaxSendOrderInfo ', res_send_mail);
             data_rep.push({
               link_invoice_preview: 'https://einvoicepro.webcashvietnam.com/lookup-einvoice?trade_code',
               lookup_code: '1234567bac',

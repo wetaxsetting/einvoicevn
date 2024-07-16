3099dea2bee2fcf36cec86763e3c4eeb13c16bd3
lvthe.zh <lvthe.zh@gmail.com>
2024-07-16 15:41:00 +0700

    ss

--- app/Controllers/Http/EInvoiceController.js
+++ app/Controllers/Http/EInvoiceController.js
@@ -7426,7 +7426,7 @@ class EInvoiceController {
         FUNC: 'weTaxSendOrderInfoV2',
         CONTENT: e.message,
       });
-      // console.log(e);
+      console.log('weTaxSendOrderInfoV2 ', e.message);
       // return response.send(Utils.response(false, e.message, null));
       return response.status(400).json(Utils.responseByRule({success: true, message: e.message}));
     }

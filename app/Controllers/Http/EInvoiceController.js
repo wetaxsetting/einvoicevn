4657dfecec5eca3f7853cf482d0528cbdbbb053b
lvthe.zh <lvthe.zh@gmail.com>
2024-07-25 16:57:18 +0700

    update ....

--- app/Controllers/Http/EInvoiceController.js
+++ app/Controllers/Http/EInvoiceController.js
@@ -17191,8 +17191,8 @@ class EInvoiceController {
 
       const {url} = request.all(); //data:6030
       if (url) {
-        let savePath = await Helpers.appRoot(url);
-        const contents = fs.readFileSync(savePath);
+        //let savePath = await Helpers.appRoot(url);
+        const contents = fs.readFileSync(url);
 
         const b64 = contents.toString('base64');
         const type = 'image/png';
--- app/Helpers/EiExcelTemplateConverter.js
+++ app/Helpers/EiExcelTemplateConverter.js
@@ -46,7 +46,7 @@ class EiExcelConverterAuto {
       //console.log("logos[0].logoPath  " , logos[0].logoPath  );
       if (logos && logos.length > 0 && logos[0].logoPath) {
         try {
-          console.log('file: EiExcelTemplateConverter.js:57 [vng-304] ExcelBuilder [vng-304] logos[0].logoPath:', logos[0].logoPath);
+          //console.log('file: EiExcelTemplateConverter.js:57 [vng-304] ExcelBuilder [vng-304] logos[0].logoPath:', logos[0].logoPath);
 
           let idimg = await exceljs.insertPathImage2(logos[0].logoPath);
           worksheet.addImage(idimg, {

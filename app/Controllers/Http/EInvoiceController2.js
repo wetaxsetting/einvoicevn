1cdb03fb5d226163fc6198d664ed2912458657eb
lvthe <46241178+lvthe@users.noreply.github.com>
2025-01-16 10:35:57 +0700

    ss

--- app/Controllers/Http/EInvoiceController2.js
+++ app/Controllers/Http/EInvoiceController2.js
@@ -3632,7 +3632,7 @@ class EInvoiceController2 {
                   message: resMess,
                 };
               }
-            } else if (key == 'buyer_email' || key == 'buyer_email_cc' || key == 'seller_email') {
+            } else if (key == 'buyer_email' || key == 'seller_email') {
               //console.log('validateJsonInvalidNormalInvoiceToXML  key   ', key, ' - ', invoice[key]);
               if (!errorList[`${key}`].test(invoice[key]) && invoice[key]) {
                 // && invoice[key]
@@ -3643,7 +3643,22 @@ class EInvoiceController2 {
                   message: resMess,
                 };
               }
-            } else if (!errorList[`${key}`].test(invoice[key])) {
+            } else if (key == 'buyer_email_cc') {
+              if (invoice[key]) {
+                let buyer_email_cc = invoice[key].split(';');
+                console.log('buyer_email_cc ', buyer_email_cc);
+                buyer_email_cc.forEach(element => {
+                  if (!errorList[`${key}`].test(element)) {
+                    status = false;
+                    resMess = `${mess1} ${key}.`;
+                    return {
+                      status,
+                      message: resMess,
+                    };
+                  }
+                });
+              }
+            }else if (!errorList[`${key}`].test(invoice[key])) {
               status = false;
               resMess = `${mess1} ${key}.`;
               return {

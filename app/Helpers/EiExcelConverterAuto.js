8ca6c62a86929047259baa43d4d64d35d4b82daa
lvthe.zh <lvthe.zh@gmail.com>
2025-01-22 10:36:07 +0700

    ss

--- app/Helpers/EiExcelConverterAuto.js
+++ app/Helpers/EiExcelConverterAuto.js
@@ -575,7 +575,7 @@ class EiExcelConverterAuto {
               } else {
                 let numHiddenRow = v_countNumberOfPages * (num_of_more_pages_max + headerRowCount);
                 //let numHiddenRow = v_countNumberOfPages * (20 + headerRowCount); // phần này lấy ra số dòng ẩn nhưng 20 dòng mới hơp lý
-                console.log('numHiddenRow ', numHiddenRow, 'num_of_more_pages_max - num_of_more_pages ', num_of_more_pages_max % num_of_more_pages);
+                //console.log('numHiddenRow ', numHiddenRow, 'num_of_more_pages_max - num_of_more_pages ', num_of_more_pages_max % num_of_more_pages);
                 for (let index = 1; index <= num_of_pages + (num_of_more_pages_max % num_of_more_pages); index++) {
                   //worksheet.getRow(totalRowCount + _sourceRow + leftCount + index + 1).hidden = true;
                   worksheet.getRow(numHiddenRow - index).hidden = true;

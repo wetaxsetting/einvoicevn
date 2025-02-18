d496c86cf56cadc384f9909c82dbf3abcdcba0a9
lvthe.zh <lvthe.zh@gmail.com>
2025-02-18 14:32:38 +0700

    ss

--- start/routes.js
+++ start/routes.js
@@ -238,7 +238,7 @@ Route.group(() => {
 Route.group(() => {
   Route.get('download-pdf-c-sharp', 'EInvoiceController.viewPDFInvoiceOut');
   Route.post('call-post-api', 'EInvoiceController.callPostAPI');
-  Route.post('call-get-api', 'EInvoiceController.callGetAPI');
+  Route.get('call-get-api', 'EInvoiceController.callGetAPI');
   Route.post('call-delete-api', 'EInvoiceController.callDeleteAPI');
 }).prefix('api/out/einvoice');
 

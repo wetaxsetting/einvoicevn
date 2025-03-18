536257830a8f6b34829bc7beb7d8753a24084db1
lvthe.zh@gmail.com <t>
2025-03-18 15:21:45 +0700

    update

--- app/Controllers/Http/EInvoiceController.js
+++ app/Controllers/Http/EInvoiceController.js
@@ -15146,7 +15146,7 @@ class EInvoiceController {
 
   convertHtmlCode(sText) {
     if (sText != null || sText == '') {
-      return this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(sText, '"', '&quot;'), '<', '&lt;'), '>', '&gt;'); //vng-199 tạm thời đóng đoạn này vì sửa lý ở C# khi ký
+      return this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(sText, '&', '&amp;'), '"', '&quot;'), '<', '&lt;'), '>', '&gt;');   
       /* this.replaceAllExt(
         this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(sText, '&', '&amp;'), '"', '&quot;'), '<', '&lt;'), '>', '&gt;'),
         "\'",
--- app/Controllers/Http/EInvoiceController2.js
+++ app/Controllers/Http/EInvoiceController2.js
@@ -3169,7 +3169,7 @@ class EInvoiceController2 {
 
   convertHtmlCode(sText) {
     if (sText != null || sText == '') {
-      return this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(sText, '"', '&quot;'), '<', '&lt;'), '>', '&gt;'); //vng-199 tạm thời đóng đoạn này vì sửa lý ở C# khi ký
+      return this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(sText, '&', '&amp;'), '"', '&quot;'), '<', '&lt;'), '>', '&gt;');  //vng-199 tạm thời đóng đoạn này vì sửa lý ở C# khi ký
       /* this.replaceAllExt(
         this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(this.replaceAllExt(sText, '&', '&amp;'), '"', '&quot;'), '<', '&lt;'), '>', '&gt;'),
         "\'",

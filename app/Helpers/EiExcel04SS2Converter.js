72a4d7febb296873b6472fca74cae3aa46cc32e3
lvthecd <lvthecd@gmail.com>
2025-07-07 13:57:28 +0700

    ss

--- app/Helpers/EiExcel04SS2Converter.js
+++ app/Helpers/EiExcel04SS2Converter.js
@@ -254,7 +254,7 @@ class EiExcelConverterAuto {
 		//"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26",
 		try {
 			worksheet.addImage(await exceljs.insertPathImage(signPath), {
-				tl: { col: 4, row: 40 },
+				tl: { col: 4, row: 42 },
 				ext: { width: 50, height: 50 }
 			});
 		} catch (error) {
